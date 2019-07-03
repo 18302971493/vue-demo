026bb6e9705b7f4c83e5a8d0bdf6bad477edbbf0
18302971493 <775771672@qq.com>
2019-07-03 16:42:27 +0800

    提交文件

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
         port: 8080,
         proxy: {
             '/sparrow': {
-                target: 'http://127.0.0.1:8020',  // 请求本地 需要xboot后台项目
+                target: 'http://127.0.0.1:8020',  // 请求本地，需要后台项目
                 ws: true,
                 changeOrigin: true,
                 pathRewrite: {
